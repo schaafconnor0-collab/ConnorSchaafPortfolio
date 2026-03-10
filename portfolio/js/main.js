59c89d1d81606703edafff8e22e81e2ba7aec400
schaafconnor0-collab <schaafconnor0@gmail.com>
2026-03-10 14:53:31 -0400

    Delete portfolio directory

--- portfolio/js/main.js
+++
@@ -1,8 +0,0 @@
-(function(){
-    const path = (window.location.pathname.split("/").pop() || "index.html").toLowerCase();
-    document.querySelectorAll(".navlinks a[data-page]").forEach(a => {
-      const p = (a.getAttribute("data-page") || "").toLowerCase();
-      if (p === path) a.classList.add("active");
-    });
-  })();
-  
